137e8897757eca1396ab9df23e2a1c4c2246117d
littledimple123 <cztian1314@163.com>
2019-10-10 15:01:12 +0800

    1

---
+++ pages/logs/logs.js
@@ -0,0 +1,31 @@
+// const app = getApp();
+Page({
+  data: {
+    list: []
+  },
+  onLoad: function() {
+    this.refreshScroll = this.selectComponent('#refreshScroll');
+    for (let i = 0; i < 100; i++) {
+      this.data.list.push(i);
+    }
+    this.setData({
+      list: this.data.list
+    });
+  },
+  onPageScroll(e) {
+    this.refreshScroll.onPageScroll(e);
+  },
+  onReachBottom() {
+    wx.showLoading({
+      title: 'onReachBottom'
+    });
+    setTimeout(() => {
+      wx.hideLoading();
+    }, 1000);
+  },
+  refresh: function(e) {
+    setTimeout(() => {
+      this.refreshScroll.stopRefresh();
+    }, 1000);
+  }
+})
