864b67784dcab0b768432283f4308aa092f872f3
littledimple123 <cztian1314@163.com>
2020-04-15 17:10:05 +0800

    map位置

--- pages/orders/orders.js
+++ pages/orders/orders.js
@@ -64,6 +64,7 @@ Page({
       dateTimeArray1: obj1.dateTimeArray,
       dateTime1: obj1.dateTime,
     });
+    
   },
   //  tab切换逻辑
   swichNav: function (e) {
@@ -113,17 +114,17 @@ Page({
     console.log(e);
   },
   // 日期时间选择器(精确到分) 下拉弹框的确定按钮
-  changeDateTime1 (e) {
+  changeDateTime1(e) {
     console.log('日期选择', e);
     this.setData({
-      dateTime1: e.detail.value
+      dateTime1: e.detail.value,
     });
   },
-  dingwei () {
+  dingwei() {
     wx.getLocation({
       type: 'gcj02',
-      success (res) {
-        console.log(res)
+      success(res) {
+        console.log(res);
         wx.chooseLocation({
           latitude: res.latitude,
           longitude: res.longitude,
@@ -132,7 +133,18 @@ Page({
           },
         });
       },
-    })
+    });
+  },
+  // 两个经纬度之间的距离
+  juli (lat1, lng1, lat2, lng2) {
+    var radLat1 = lat1 * Math.PI / 180.0;
+    var radLat2 = lat2 * Math.PI / 180.0;
+    var a = radLat1 - radLat2;
+    var b = lng1 * Math.PI / 180.0 - lng2 * Math.PI / 180.0;
+    var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+    s = s * 6378.137;
+    s = Math.round(s * 10000) / 10000;
+    return s
   },
   //转发
   onShareAppMessage() {},
